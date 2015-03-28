6d2e759e08853322666420a4b98dcb83bc289625
piglikeyoung <316046510@qq.com>
2015-03-28 12:23:02 +0800

    Initial Commit

---
+++ 黑马微博/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  黑马微博
+//
+//  Created by piglikeyoung on 15/3/28.
+//  Copyright (c) 2015年 jinheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
