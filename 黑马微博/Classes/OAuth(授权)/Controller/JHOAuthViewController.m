ab978effe16dc8f3abc0000c15f6a23d9fec72dc
piglikeyoung <316046510@qq.com>
2015-04-02 12:38:31 +0800

    存储账号信息

--- 黑马微博/Classes/OAuth(授权)/Controller/JHOAuthViewController.m
+++ 黑马微博/Classes/OAuth(授权)/Controller/JHOAuthViewController.m
@@ -8,6 +8,9 @@
 
 #import "JHOAuthViewController.h"
 #import "MBProgressHUD+MJ.h"
+#import "AFNetworking.h"
+#import "JHTabBarViewController.h"
+#import "JHNewfeatureViewController.h"
 
 @interface JHOAuthViewController () <UIWebViewDelegate>
 
@@ -90,8 +93,7 @@
         NSString *code = [url substringFromIndex:from];
         
         // 根据code获得一个accessToken
-//        [self accessTokenWithCode:code];
-        JHLog(@"code=%@",code);
+        [self accessTokenWithCode:code];
         
         // 禁止加载回调页面
         return NO;
@@ -101,13 +103,65 @@
 }
 
 /**
- *  根据code获得一个accessToken
+ *  根据code获得一个accessToken(发送一个POST请求)
  *
  *  @param code 授权成功后的请求标记
  */
 - (void)accessTokenWithCode:(NSString *)code
 {
-    //处理操作....
+    // 1.获得请求管理者
+    AFHTTPRequestOperationManager *mgr = [AFHTTPRequestOperationManager manager];
+    
+    // 2.封装请求参数
+    NSMutableDictionary *params = [NSMutableDictionary dictionary];
+    params[@"client_id"] = @"1359433872";
+    params[@"client_secret"] = @"37c372aa97a9329fc561947151c1bd38";
+    params[@"redirect_uri"] = @"http://ios.itcast.cn";
+    params[@"grant_type"] = @"authorization_code";
+    params[@"code"] = code;
+    
+    // 3.发送POST请求
+    [mgr POST:@"https://api.weibo.com/oauth2/access_token" parameters:params success:^(AFHTTPRequestOperation *operation, NSDictionary *responseObject) {
+        // 隐藏HUD
+        [MBProgressHUD hideHUD];
+        
+        JHLog(@"请求成功--%@", responseObject);
+        
+        // 存储授权成功的帐号信息
+        NSString *doc = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+        NSString *filePath = [doc stringByAppendingPathComponent:@"account.plist"];
+        [responseObject writeToFile:filePath atomically:YES];
+        
+        // 切换控制器，可能去新特性\tabbar
+        NSString *versionKey = (__bridge NSString *)kCFBundleVersionKey;
+        
+        // 从沙盒中取出上次存储的软件版本号(取出用户上次的使用记录)
+        NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+        NSString *lastVersion = [defaults objectForKey:versionKey];
+        
+        // 获得当前打开软件的版本号
+        NSString *currentVersion = [NSBundle mainBundle].infoDictionary[versionKey];
+        
+        UIWindow *window = [UIApplication sharedApplication].keyWindow;
+        if ([currentVersion isEqualToString:lastVersion]) {
+            // 当前版本号 == 上次使用的版本：显示JHTabBarViewController
+            window.rootViewController = [[JHTabBarViewController alloc] init];
+        } else {
+            // 当前版本号 != 上次使用的版本：显示版本新特性
+            window.rootViewController = [[JHNewfeatureViewController alloc] init];
+        
+            // 存储这次使用的软件版本
+            [defaults setObject:currentVersion forKey:versionKey];
+            [defaults synchronize];
+        }
+        
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        // 隐藏HUD
+        [MBProgressHUD hideHUD];
+        
+        JHLog(@"请求失败--%@", error);
+
+    }];
 }
 
 
