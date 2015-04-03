aa39d735270f94d685f9417e61280c2276a8ead9
piglikeyoung <316046510@qq.com>
2015-04-03 17:55:50 +0800

    发微博界面实现自定义UITextView，实现有提示文字效果

--- 黑马微博/Classes/Compose(发微博)/Controller/JHComposeViewController.m
+++ 黑马微博/Classes/Compose(发微博)/Controller/JHComposeViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "JHComposeViewController.h"
+#import "JHTextView.h"
 
 @interface JHComposeViewController ()
 
@@ -18,12 +19,36 @@
 {
     [super viewDidLoad];
     
+    // 设置导航条内容
+    [self setupNavBar];
+    
+    // 添加输入控件
+    [self setupTextView];
+}
+
+// 添加输入控件
+- (void)setupTextView
+{
+    // 1.创建输入控件
+    JHTextView *textView = [[JHTextView alloc] init];
+    textView.frame = self.view.bounds;
+    [self.view addSubview:textView];
+    
+    // 2.设置提醒文字（占位文字）
+    textView.placehoder = @"分享新鲜事...";
+    
+    // 3.设置字体
+    textView.font = [UIFont systemFontOfSize:15];
+}
+
+// 设置导航条内容
+- (void)setupNavBar
+{
     self.title = @"发微博";
     self.view.backgroundColor = [UIColor whiteColor];
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"取消" style:UIBarButtonItemStylePlain target:self action:@selector(cancel)];
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"发送" style:UIBarButtonItemStylePlain target:self action:@selector(send)];
     self.navigationItem.rightBarButtonItem.enabled = NO;
-
 }
 
 /**
